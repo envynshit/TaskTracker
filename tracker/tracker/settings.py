9d17368009ff70eab20b6ef90d07a3a360d42211
envynshit <russelvillamil253@gmail.com>
2025-12-20 13:46:33 +0800

    STATIC ROOT

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -126,5 +126,5 @@ import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "tracker", "staticfiles")
 
